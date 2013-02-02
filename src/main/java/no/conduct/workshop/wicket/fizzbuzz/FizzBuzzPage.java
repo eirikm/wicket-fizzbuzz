3a59b95d04453f4bb55bfdc7177014204582643c
Eirik Meland <eirikm@conduct.no>
2013-02-02 21:51:20 +0100

    add ajaxLink for incrementing number

--- src/main/java/no/conduct/workshop/wicket/fizzbuzz/FizzBuzzPage.java
+++ src/main/java/no/conduct/workshop/wicket/fizzbuzz/FizzBuzzPage.java
@@ -1,12 +1,24 @@
 package no.conduct.workshop.wicket.fizzbuzz;
 
+import org.apache.log4j.Logger;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.Model;
 
 public class FizzBuzzPage extends WebPage {
+    public static final Logger LOG = Logger.getLogger(FizzBuzzPage.class);
+
     public FizzBuzzPage() {
         add(new NumberPanel("numberPanel", Model.of(0)));
         add(new FizzPanel("fizzPanel", Model.of(0)));
         add(new BuzzPanel("buzzPanel", Model.of(0)));
+
+        add(new AjaxLink<Void>("ajaxLink") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                LOG.debug("click");
+            }
+        }.setBody(Model.of("Neste tall")));
     }
 }
