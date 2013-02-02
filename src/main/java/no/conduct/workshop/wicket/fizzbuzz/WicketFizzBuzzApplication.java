ff772b3c6380ecb211f4807dee2f40730f786e5e
Eirik Meland <eirikm@conduct.no>
2013-02-03 00:13:38 +0100

    add "don't change this"-comments

--- src/main/java/no/conduct/workshop/wicket/fizzbuzz/WicketFizzBuzzApplication.java
+++ src/main/java/no/conduct/workshop/wicket/fizzbuzz/WicketFizzBuzzApplication.java
@@ -1,9 +1,11 @@
 package no.conduct.workshop.wicket.fizzbuzz;
 
-import no.conduct.workshop.wicket.fizzbuzz.FizzBuzzPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
+/**
+ * Denne skal ikke endres
+ */
 public class WicketFizzBuzzApplication extends WebApplication {
     @Override
     public Class<? extends WebPage> getHomePage() {
