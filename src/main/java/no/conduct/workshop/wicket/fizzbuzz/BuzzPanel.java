2a6aebdc4585cfbc5f0d9d5bf345c97bddd36c22
Eirik Meland <eirikm@conduct.no>
2013-02-03 00:14:06 +0100

    add logger

--- src/main/java/no/conduct/workshop/wicket/fizzbuzz/BuzzPanel.java
+++ src/main/java/no/conduct/workshop/wicket/fizzbuzz/BuzzPanel.java
@@ -1,10 +1,13 @@
 package no.conduct.workshop.wicket.fizzbuzz;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
 public class BuzzPanel extends Panel {
+    private static final Logger LOG = Logger.getLogger(BuzzPanel.class);
+
     public BuzzPanel(String id, Model<Integer> model) {
         super(id);
         add(new Label("buzz", "-"));
--- src/main/java/no/conduct/workshop/wicket/fizzbuzz/FizzPanel.java
+++ src/main/java/no/conduct/workshop/wicket/fizzbuzz/FizzPanel.java
@@ -1,10 +1,13 @@
 package no.conduct.workshop.wicket.fizzbuzz;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
 public class FizzPanel extends Panel {
+    private static final Logger LOG = Logger.getLogger(FizzPanel.class);
+
     public FizzPanel(String id, Model<Integer> model) {
         super(id);
         add(new Label("fizz", "-"));
